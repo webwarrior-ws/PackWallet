6d002cd35de3b94a8f588fd13295d1599dbda3b9
siwatanejo <7511840+siwatanejo@users.noreply.github.com>
2024-03-10 18:38:25 +0100

    Integration tests against LDK bindings

---
+++ src/PackWallet.Tests.Integration/LdkInterop.cs
@@ -0,0 +1,25 @@
+﻿namespace PackWallet.Tests.Integration;
+
+using org.ldk.structs;
+
+class ConsoleLogger : LoggerInterface
+{
+    public void log(Record record)
+    {
+        // just don't throw
+    }
+}
+
+public class LdkInterop
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestLoggerDoesntCrash()
+    {
+        Logger.new_impl(new ConsoleLogger());
+    }
+}
